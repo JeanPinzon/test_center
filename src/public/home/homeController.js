b87866770e9ed4472700db608ecb26227fe634b8
Jean Pinzon <jeanpinzon@icloud.com>
2016-05-03 21:41:03 -0300

    iniciando homeController

--- src/public/home/homeController.js
+++ src/public/home/homeController.js
@@ -3,6 +3,43 @@
 
   var homeController = function () {
 
+    var self = this;
+
+    self.defaultOptions = [{
+      text: 'Selecione'
+    },{
+      value: 1,
+      text: '1 - Não atende ao requisito'
+    },{
+      value: 2,
+      text: '2 - Abaixo da média'
+    },{
+      value: 3,
+      text: '3 - Normal'
+    },{
+      value: 4,
+      text: '4 - Acima do desejado'
+    },{
+      value: 5,
+      text: '5 - Excelente'
+    }];
+
+    self.automatedTestsOptions = [{
+      text: 'Selecione'
+    },{
+      value: 0,
+      text: '0% - Péssimo'
+    },{
+      value: 20,
+      text: '20% - Ruim'
+    },{
+      value: 50,
+      text: '50% - OK'
+    },{
+      value: 70,
+      text: '70% - Excelente'
+    }];
+
   };
 
   angular.module('testCenter').controller('homeController', [
