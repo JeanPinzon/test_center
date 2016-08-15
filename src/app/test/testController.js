841ea7e1ba54dc29106f324673f9ea14b410b319
Rafael de Oliveira Benetti <rafaeldeoliveirabenetti@gmail.com>
2016-08-15 00:12:25 -0300

    add feedback to e-mail and adjusting preview height on page

--- src/app/test/testController.js
+++ src/app/test/testController.js
@@ -90,17 +90,17 @@
     controller.send = function (req, res) {
       var html = buildResult(req.body);
       sendEmail(html);
-      res.json({ ok: true, html: html });
+      res.json({ valid: true, html: html });
     };
 
     controller.preview = function (req, res) {
       var html = buildResult(req.body);
-      res.json({ ok: true, html: html });
+      res.json({ valid: true, html: html });
     };
 
     controller.getLevel = function(req, res) {
       var test = validate(req.body);
-      res.json({ ok: true, level: test.level });
+      res.json({ valid: true, level: test.level });
     };
 
     var sendEmail = function(result) {
@@ -207,6 +207,7 @@
       html = html.replace("[CANDIDATE_NAME]", test.name)
       .replace("[CANDIDATE_LEVEL]", test.level)
       .replace("[CANDIDATE_LEVEL_IMAGE]", test.image)
+      .replace("[CANDIDATE_FEEDBACK]", test.feedback)
 
       .replace("[UNIT_TESTS.NOTE]", test.level)
       .replace("[UNIT_TESTS.DESCRIPTION]", test.level)
--- src/public/home/homeController.js
+++ src/public/home/homeController.js
@@ -87,7 +87,7 @@
     self.preview = function () {
       self.getTextValues();
       testService.preview(self.test);
-      self.test.isViewed = true;
+      self.test.isViewed = true;      
     };
 
     self.getLevelByDescription = function(description){
@@ -108,7 +108,6 @@
     };
 
     self.canFinish= function(){
-
       return self.canPreview() && !!self.test && self.test.isViewed;
     };
 
