efaa3140acb8d3bccf0184b7cef63baddb75f52a
Rafael de Oliveira Benetti <rafaeldeoliveirabenetti@gmail.com>
2016-11-11 09:18:05 -0200

    rename controller file

---
+++ src/app/test/testController.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  var EmailService = require('../core/email/EmailService');
+  var emailService = new EmailService();
+
+  var TestService = require('./TestService');
+  var testService = new TestService();
+
+  module.exports = function (app) {
+
+    var controller = {};
+
+    controller.send = function (req, res) {
+      var html = testService.buildResult(req.body);
+      emailService.send(html);
+      res.json({ valid: true });
+    };
+
+    controller.preview = function (req, res) {
+      var html = testService.buildResult(req.body);
+      res.json({ valid: true, html: html });
+    };
+
+    controller.getLevel = function(req, res) {
+      var test = testService.validate(req.body);
+      res.json({ valid: true, level: test.level });
+    };
+
+    return controller;
+  };
+})();
